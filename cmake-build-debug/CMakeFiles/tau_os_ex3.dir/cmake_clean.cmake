f0be21b4b3fc729bffc285ce9678b4e356404d78
exx8 <exx8eran@gmail.com>
2020-12-06 16:00:48 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/tau_os_ex3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tau_os_ex3.dir/main.c.o"
+  "tau_os_ex3"
+  "tau_os_ex3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tau_os_ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
