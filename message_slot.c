46c94d580400aae2a012aecb8358e943fbcccfe1
exx8 <exx8eran@gmail.com>
2020-12-19 12:28:36 +0200

    remove debug. printk and debug_pointer

--- message_slot.c
+++ message_slot.c
@@ -21,14 +21,8 @@ typedef struct {
 
 static int size_of_lists[channel_num];
 static  msg **minor_arr;
-static void debug(char *const fmt) {
-    return;
-    printk(KERN_ERR "%s", fmt); }
-    static void debug_pointer(void * pointer)
-    {
-    return;
-    printk(KERN_ERR "%p",pointer);
-    }
+
+
     /**
      *
      * @param minor
@@ -41,7 +35,6 @@ int  add2list( int minor, msg  new_msg)
     void *newPlace;
     printk(KERN_ERR "minor in add2list is %d",minor);
     size_of_lists[minor]++;
-    debug("pre-realloc");
     newPlace= krealloc(minor_arr[minor], size_of_lists[minor]*sizeof(msg ), GFP_KERNEL);
     printk("resize complete");
     if(newPlace==NULL) {
@@ -82,27 +75,21 @@ static int device_open(struct inode *inode, struct file *file) {
 }
 
 static bool no_channel(const struct file *file) {
-    debug("in no channel");
 
     return ((private_data_type*)file->private_data)->channel_id==NO_CHANNEL; }
 
 static msg *get_entry_by_channel_id(const char *buffer, unsigned int channel_id,unsigned minor) {
     int i=0;
-    printk("size as viewed by get_entry_by_channel_id :%d",size_of_lists[minor]);
-    debug_pointer(minor_arr[minor]);
     for(i=size_of_lists[minor]-1;i>=0;i--)
     {
     msg entry =( minor_arr[minor])[i];
     if (entry.channel_id == channel_id) {
-        debug(entry.msg_value);
 
         return &minor_arr[minor][i];
 
 
         }
     }
-    printk(KERN_ERR "postmortum");
-    debug_pointer(size_of_lists);//to eliminate warnings
     return NULL;
 }
 
@@ -113,19 +100,14 @@ static ssize_t device_read(struct file *file, char __user *buffer, size_t length
     msg *entry;
     int channel_id;
 
-    debug("before no channel read");
     if (no_channel(file)){
-        debug("found no channel");
         return -EINVAL;
         }
-    debug("before reading channel_id");
     channel_id = ((private_data_type*)file->private_data)->channel_id;
     minor=((private_data_type*)file->private_data)->minor;
-    printk("read minor: %d",minor);
     entry = get_entry_by_channel_id(buffer, channel_id,minor);
 
     if (entry == NULL) {
-        debug("in EWOULDBLOCK");
         return -EWOULDBLOCK; //resource temproray unavailable
     }
 
@@ -149,36 +131,28 @@ static ssize_t device_write(struct file *file, const char __user *buffer, size_t
     int minor;
     int channel_id;
     int status;
-    printk(KERN_ERR "write start, private data address is %p",file->private_data);
     minor = ((private_data_type*)file->private_data)->minor;
-    printk("shouldn't be 0: %d",((private_data_type*)file->private_data)->minor);
     if (no_channel(file))
         return -EINVAL;
-    printk("is buffer NULL %d",buffer==NULL);
     if (buffer == NULL)
         return -EINVAL;
-    debug("device write");
     channel_id = ((private_data_type*)file->private_data)->channel_id;
-    debug("device write minor");
     if (length == 0 || length > msg_len)
         return -EMSGSIZE;
 
     new_msg.channel_id=channel_id;
     new_msg.len=length;
     priv_buffer = new_msg.msg_value;
-    debug("device write before for");
-    printk("%zu",length);
+
     for (i = 0; i < length; i++) {
         int status_get_user=get_user(priv_buffer[i], &buffer[i]);
         if(status_get_user!=0)
             return -EFAULT;
     }
-    debug("device write before add2list");
 
     status=add2list(minor,new_msg);
     if(!status)
         return -ENOMEM;
-    printk("viewed length is :%d,status is :%d",size_of_lists[minor],status);
     return i;
 }
 
@@ -190,7 +164,6 @@ static long einvalid_ioctl(void) {
 
 static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsigned long channel) {
     unsigned int channel_id = channel;
-    debug("iocntrl\n");
     if (ioctl_command_id != MSG_SLOT_CHANNEL || channel == 0) {
         return einvalid_ioctl();
     }
@@ -226,24 +199,15 @@ static int __init simple_init(void) {
     int major;
     int status;
     // Register driver capabilities. Obtain major num
-    printk("%d",MAJOR_NUM);
     status=register_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME, &Fops);
     major=MAJOR_NUM;
     // Negative values signify an error
     if (status < 0) {
-        printk(KERN_ALERT "%s registraion failed for  %d\n",
-               DEVICE_FILE_NAME, major);
+
         return status;
     }
 
-    printk("Registeration is successful. "
-           "The major device number is %d.\n", major);
-    printk("If you want to talk to the device driver,\n");
-    printk("you have to create a device file:\n");
-    printk("mknod /dev/%s c %d 0\n", DEVICE_FILE_NAME, major);
-    printk("You can echo/cat to/from the device file.\n");
-    printk("Dont forget to rm the device file and "
-           "rmmod when you're done\n");
+
 
     return 0;
 }
@@ -254,7 +218,6 @@ static void __exit simple_cleanup(void) {
     int i;
     if(minor_arr!=NULL) {
         for (i = 0; i < channel_num; i++) {
-            printk(KERN_ERR"freeing the first %d", i);
             if (minor_arr[i] != NULL)
                 kfree(minor_arr[i]);
             size_of_lists[i] = 0;
